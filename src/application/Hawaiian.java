667e215a064d3d26533ae7551f74fb0545a1621f
Nerr4life <84106087+Nerr4life@users.noreply.github.com>
2021-11-11 11:34:11 -0500

    Add files via upload

---
+++ src/application/Hawaiian.java
@@ -0,0 +1,3 @@
+public class Hawaiian extends Pizza{
+    
+}
---
+++ src/application/Pizza.java
@@ -0,0 +1,5 @@
+public abstract class Pizza{
+    protected ArrayList<Topping> toppings = new ArrayList<Topping>();
+    protected Size size;
+    public abstract double price();
+}
