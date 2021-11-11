4c683490508ad911c28c0e89c2a29c801a7775b6
Nerr4life <84106087+Nerr4life@users.noreply.github.com>
2021-11-11 15:36:02 -0500

    Update Deluxe.java

--- src/application/Deluxe.java
+++ src/application/Deluxe.java
@@ -1,3 +1,8 @@
 public class Deluxe extends Pizza{
-    
+    public static final double basePrice = 12.99;
+    public static final double sizeIncrease = 2.00;
+    public static final double toppingIncrease = 1.49;
+    public static final double taxRate = 1.06625;
+    public static final int toppingCap = 7;
+    public static final int numOfToppings = 5
 }
