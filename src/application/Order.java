733795ac7436060b7a5850055279d940bef077b2
Jeevan Vasanthan <jeevan.vasanthan@gmail.com>
2021-11-13 17:34:41 -0500

    Update

--- src/application/Order.java
+++ src/application/Order.java
@@ -1,14 +1,38 @@
+package application;
+
+import java.util.ArrayList;
+
 public class Order {
     
-    public void addPizza(){
-        
+	private ArrayList<Pizza> pizzas;
+	public String phoneNumber;
+	
+	public Order(String phoneNumber) {
+		pizzas = new ArrayList<>();
+		this.phoneNumber = phoneNumber;
+	}
+	
+    public void addPizza(String flavor){
+    	 pizzas.add(PizzaMaker.createPizza(flavor));
     }
     
-    public void removePizza(){
-        
+    public void removePizza(Pizza pizza){
+        pizzas.remove(pizza);
     }
     
-    public String toString(){
-        
+    public boolean isValid(){
+    	
+    	if(phoneNumber.length() != 10)
+    		return false;
+    	try {
+    		Integer.parseInt(phoneNumber);
+    	}
+    	catch(NumberFormatException e) {
+    		return false;
+    	}
+    	
+    	return true;
     }
+    
+   
 }
--- src/application/PizzaMaker.java
+++ src/application/PizzaMaker.java
@@ -2,6 +2,12 @@ package application;
 
 public class PizzaMaker {
     public static Pizza createPizza(String flavor){
-        
+    	
+        if(flavor.equals("Hawaiian"))
+        	return new Hawaiian();
+        else if(flavor.equals("Deluxe"))
+        	return new Deluxe();
+        else
+        	return new Pepperoni();
     }
 }
