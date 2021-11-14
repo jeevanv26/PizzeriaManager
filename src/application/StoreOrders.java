9126a010d1fe34fe133278d476083fde5a3fc61c
Nerr4life <84106087+Nerr4life@users.noreply.github.com>
2021-11-14 11:48:03 -0500

    Update StoreOrders.java

--- src/application/StoreOrders.java
+++ src/application/StoreOrders.java
@@ -1,5 +1,26 @@
 package application;
 
 public class StoreOrders {
-    
+
+    private ArrayList<Order> orders;
+
+    public StoreOrders() {
+        orders = new ArrayList<>;
+    }
+
+    public void cancelOrder(Order order) {
+        orders.remove(order);
+    }
+
+    public void addOrder(Order order) {
+        orders.add(order);
+    }
+
+    public String toString() {
+
+    }
+
+    public void export() {
+
+    }
 }
